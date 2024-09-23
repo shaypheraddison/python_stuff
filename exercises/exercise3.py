16dce51f67332a9c4b668f85fc3eaa46294d4c74
Shaypher Ashe <149912769+shaypheraddison@users.noreply.github.com>
2024-09-22 23:09:48 -0500

    Update exercise3.py

--- exercises/exercise3.py
+++ exercises/exercise3.py
@@ -1,10 +1,5 @@
 # create a deck of cards where the cards are a dictionary
-values = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
-suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
-for value in values:
-    for suit in suits:
-        card = {"Suit": suit, "Value" : value}
-        print(card)
+
 
 # card = {
 #     "value": "King",
