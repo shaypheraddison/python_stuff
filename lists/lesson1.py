52831917db52b4954e56fe6c88c81fc82ea7a6e9
shaypheraddison <149912769+shaypheraddison@users.noreply.github.com>
2024-09-23 09:13:01 -0500

    completed lesson 1

--- lists/lesson1.py
+++ lists/lesson1.py
@@ -1,11 +1,21 @@
 # create an empty lists
+empty = []
 
 # create a list with strings
+strings = ["This", "is", "a", "list"]
 
 # create a list with ints
+ints = [10, 20, 30, 40]
 
 # create a list with bool
+bools = [True, True, False]
 
 # create a list with floats
+floats = [1.1, 1.2, 1.3, 1.4]
 
 # create a list within a list
+lists = [
+    [10, 20, 30],
+    [40, 50, 60],
+    [70, 80, 90]
+]
